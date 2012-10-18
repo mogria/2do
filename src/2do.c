07c3d10951fb99d60f5b4171b20f42f792d01165
Mogria <m0gr14@gmail.com>
2012-10-18 22:31:48 +0200

    Initial Commit

---
+++ src/2do.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include "2do.h"
+
+
+int main(int argc, char **argv) {
+
+  puts(USAGE);
+
+  return EXIT_SUCCESS;
+}
---
+++ src/2do.h
@@ -0,0 +1,17 @@
+#ifndef _2DO_H
+#define _2DO_H
+
+#define VERSION_MAJOR "0"
+#define VERSION_MINOR "1"
+#define VERSION_PATCH "0"
+#define VERSION_APPENDIX "-alpha"
+
+#define VERSION VERSION_MAJOR "." VERSION_MINOR "." VERSION_PATCH VERSION_APPENDIX
+
+#define USAGE "2do v" VERSION "\n" \
+              "\n" \
+              "Usage:\n" \
+              "  - none\n" \
+              "\n"
+
+#endif /* _2DO_H */
