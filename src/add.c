faaa61df8e0a5e0528566599561805ea90fab748
Mogria <m0gr14@gmail.com>
2012-10-19 20:09:15 +0200

    updated "2do add" command. It now removes empty arguments automaticly. Throws an exception if no message given

--- src/add.c
+++ src/add.c
@@ -1,11 +1,28 @@
 #include "add.h"
+#include "exception.h"
 #include "todo-file.h"
 
 int add_command(int argc, char **argv) {
   FILE *todo_file = open_todo_file("a");
-  int i;
+  int i, copy_by = 0;
   char append = ' ';
 
+  // remove empty paramters
+  for(i = 0; i < argc; i++) {
+    argv[i - copy_by] = argv[i];
+    if(strcmp(argv[i], "") == 0) {
+      copy_by++;
+    } 
+  }
+  argc -= copy_by;
+
+  // if no or only empty arguments are given, throw an exception
+  if(!argc) {
+    fclose(todo_file);
+    THROW("no message given", 2);
+  }
+
+  // else write new line info file
   for(i = 0; i < argc; i++) {
     if(i + 1 == argc) {
       append = '\n';
