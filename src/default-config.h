cfa7c85dfa486c3b823e2416e06859d8ce0f8239
Mogria <m0gr14@gmail.com>
2012-11-07 02:02:25 +0100

    moved config file specification to default-config.h

--- src/default-config.h
+++ src/default-config.h
@@ -1,6 +1,11 @@
 #ifndef _DEFAULT_CONFIG_H
 #define _DEFAULT_CONFIG_H
 
+#include "config.h"
+
 #define DEFAULT_FILENAME "todo.txt"
+#define CONFIG_FILE ".2dorc"
+
+void initialize_config(struct config *config);
 
 #endif
