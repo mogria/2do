01cf71024f78c453c0c3680dc8ed071fae312456
Mogria <m0gr14@gmail.com>
2012-11-22 19:52:08 +0100

    fixed '2do list' command but lost automatic wrap functionality if line gets longer than specified

--- src/commands/list.c
+++ src/commands/list.c
@@ -3,17 +3,14 @@
 #include "../todo-file/todo-file.h"
 #include "../file/filebuffer.h"
 #include "../config/config.h"
+#include <mcstring/mcstring.h>
 #include <string.h>
 
 int list_command(int argc, char **argv) {
   filebuffer file;
   char *line;
-  size_t i, u;
-  size_t maxsize = 4;
-  int max_linesize = get_config()->max_linesize;
-  int linepos = 0;
-  int current_linesize = 0;
-  unsigned char printed_number = 0;
+  char *current_line;
+  size_t i, maxsize;
   FILE *todo_file = open_todo_file("r");
 
   read_file(&file, todo_file);
@@ -27,7 +24,7 @@ int list_command(int argc, char **argv) {
   maxsize += 6;
  
   ALLOC1(char, line, maxsize + 1);
-  memset(line, '-', max_linesize <= 10 ? maxsize : max_linesize);
+  memset(line, '-', maxsize);
 
   if(get_config()->list_markdown_table) {
     line[3] = ':';
@@ -39,25 +36,12 @@ int list_command(int argc, char **argv) {
   
   printf("%3s | %s\n", "#", "task");
   printf("%s\n", line);
+  FREE1(line);
+
   for(i = 0; i < file.num_lines; i++) {
-    printed_number = 0;
-    linepos = 0;
-    current_linesize = max_linesize <= 10 ? string_size(file.lines[i]) : max_linesize - 6;
-    copy_string(line, file.lines[i], current_linesize);
-    do {
-      if(!printed_number) {
-        printf("%3u | ", (unsigned int)i + 1);
-        printed_number = 1;
-      } else {
-        printf("    | ");
-      }
-      for(u = 0; u < current_linesize && u + linepos < string_size(file.lines[i]); u++) {
-        printf("%c", line[linepos + u]);
-      }
-      puts("");
-      linepos += current_linesize;
-    } while(linepos < string_size(file.lines[i]));
-    free(line);
+    current_line = c_string(file.lines[i]);
+    printf("%3u | %s\n", (unsigned int)i, line);
+    free(current_line);
   }
 
   free_filebuffer(&file);
