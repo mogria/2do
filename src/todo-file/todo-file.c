c6b3299ee4b7efff090fc805b938fd7e1189e80a
Mogria <m0gr14@gmail.com>
2012-11-23 01:03:10 +0100

    updated remove_line_by_number due new filebuffer

--- src/todo-file/todo-file.c
+++ src/todo-file/todo-file.c
@@ -2,6 +2,7 @@
 #include "../exception.h"
 #include "../array.h"
 #include "../file/file.h"
+#include "../file/filebuffer.h"
 #include "../config/config.h"
 #include <stdio.h>
 
@@ -16,21 +17,20 @@ FILE *open_todo_file(const char *mode) {
 }
 
 void remove_line_by_number(FILE *todo_file, int number) {
-  char **linebuffer;
-  size_t *linesizes;
-  size_t size;
-  size_t i;
+  filebuffer file;
+  int i;
 
-  read_file(&linebuffer, &linesizes, &size, todo_file);
+  read_file(&file, todo_file);
 
   truncate_file(todo_file);
 
-  for(i = 0; i < size; i++) {
+  for(i = 0; i < file.num_lines; i++) {
     if(i + 1 != number) {
-      fprintf(todo_file, "%s\n", linebuffer[i]);
+      char *line = c_string(file.lines[i]);
+      fprintf(todo_file, "%s\n", line);
+      free(line);
     }
   }
 
-  FREE2(linebuffer, size);
-  FREE1(linesizes);
+  free_filebuffer(&file);
 }
