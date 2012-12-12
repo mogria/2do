79e07f16a4e0565cb172022c6ad04a031d18c60d
Mogria <m0gr14@gmail.com>
2012-12-12 02:04:20 +0100

    implemented functions to remove and add lines to a filebuffer

--- src/file/filebuffer.c
+++ src/file/filebuffer.c
@@ -2,6 +2,7 @@
 #include "../array.h"
 #include <stddef.h>
 #include <stdio.h>
+#include <string.h>
 
 void write_file(filebuffer *buffer, FILE *file) {
   int x;
@@ -16,13 +17,12 @@ void write_file(filebuffer *buffer, FILE *file) {
 void read_file(filebuffer *buffer, FILE *file) {
   char c;
   char *line;
-  buffer->num_lines = 0;
   size_t linesize = 1;
 
   // go back to the start of the file
   rewind(file);
 
-  ALLOC1(mcstring *, buffer->lines, buffer->num_lines);
+  filebuffer_init(buffer);
   ALLOC1(char, line, 1);
 
   // iterate through the file until the end
@@ -31,9 +31,8 @@ void read_file(filebuffer *buffer, FILE *file) {
     // create a new element to the buffer if a new line begins
     if(c == '\n') {
       line[linesize] = '\0';
+      filebuffer_append(buffer, new_string(line));
       linesize = 1;
-      RALLOC1(mcstring *, buffer->lines, ++buffer->num_lines);
-      buffer->lines[buffer->num_lines - 1] = new_string(line);
       RALLOC1(char, line, linesize);
     } else {
       linesize++;
@@ -45,9 +44,56 @@ void read_file(filebuffer *buffer, FILE *file) {
   FREE1(line);
 }
 
-void free_filebuffer(struct filebuffer *buffer) {
+void filebuffer_init(filebuffer *buffer) {
   buffer->num_lines = 0;
-  FREE1(buffer->lines);
+  ALLOC1(mcstring *, buffer->lines, buffer->num_lines);
 }
 
+void filebuffer_append(filebuffer *buffer, mcstring *line) {
+  RALLOC1(mcstring *, buffer->lines, ++buffer->num_lines);
+  buffer->lines[buffer->num_lines - 1] = line;
+}
 
+void filebuffer_clear(filebuffer *buffer) {
+  free_filebuffer(buffer);
+  filebuffer_init(buffer);
+}
+
+unsigned char filebuffer_remove(filebuffer *buffer, int line_number) {
+  // linenumber == 0 -> clear()
+  // linenumber > 0 -> remove line with this number
+  // linenumber < 0 -> same as above, but relative
+  //                   to the end of the file
+
+  unsigned char ret = 1;
+  if(line_number == 0) {
+    filebuffer_clear(buffer);
+  } else {
+    if(line_number < 0) {
+      line_number = buffer->num_lines - line_number + 1;
+    }
+    line_number--;
+    
+    if((ret = (line_number >= 0 && line_number < buffer->num_lines))) {
+      memmove(&buffer->lines[line_number],
+        &buffer->lines[line_number + 1],
+        sizeof(mcstring*) * (buffer->num_lines - 1 - line_number));
+      RALLOC1(mcstring *, buffer->lines, ++buffer->num_lines);
+    }
+  }
+
+  return ret;
+}
+
+void free_filebuffer(filebuffer *buffer) {
+  int x;
+  for(x = 0; x < buffer->num_lines; x++) {
+    free_string(buffer->lines[x]);
+  }
+  only_free_filebuffer(buffer);
+}
+
+void only_free_filebuffer(filebuffer *buffer) {
+  FREE1(buffer->lines);
+  buffer->num_lines = 0;
+}
--- src/file/filebuffer.h
+++ src/file/filebuffer.h
@@ -10,6 +10,11 @@ typedef struct filebuffer {
 
 void write_file(filebuffer *buffer, FILE *file);
 void read_file(filebuffer *buffer, FILE *file);
+void filebuffer_init(filebuffer *buffer);
+void filebuffer_append(filebuffer *buffer, mcstring *line);
 void free_filebuffer(filebuffer *buffer);
+void filebuffer_clear(filebuffer *buffer);
+unsigned char filebuffer_remove(filebuffer *buffer, int line_number);
+void only_free_filebuffer(filebuffer *buffer);
 
 #endif /* _FILEBUFFER_H */
